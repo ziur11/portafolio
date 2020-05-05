941378ec7f22d7357396279a29463ca5914f2334
Jesus Ruiz <ziur11@gmail.com>
2020-05-05 22:00:53 +0200

    fin clase 44

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+
+    this.route.params
+    .subscribe( parametros => {
+
+      // tslint:disable-next-line: no-string-literal
+      console.log(parametros['id']);
+
+    });
   }
 
 }
